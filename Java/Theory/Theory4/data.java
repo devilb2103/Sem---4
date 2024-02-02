b318e097a5f1f852a14fc4ad36434bdea67c0cd1
Dev Bhanushali <devbhanushali21@gmail.com>
2024-02-03 01:11:37 +0530

    .

--- Java/Theory/Theory4/data.java
+++ Java/Theory/Theory4/data.java
@@ -2,7 +2,7 @@ package Java.Theory.Theory4;
 
 public class data {
     public static void main(String[] args){
-        data2 x = new data2();
+        data2 x = new data2();;
         // new data2();
         // // new data2();
         // // new data2();
